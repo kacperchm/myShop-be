ebec6959b5868c199c8af014c1994a54c6422583
kacperchm <chmuraka1998@gmail.com>
2024-01-22 22:56:15 +0100

    Add hashing password

--- auth/src/main/java/com/kacperchm/auth/services/UserService.java
+++ auth/src/main/java/com/kacperchm/auth/services/UserService.java
@@ -3,6 +3,7 @@ package com.kacperchm.auth.services;
 import com.kacperchm.auth.entity.User;
 import com.kacperchm.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,10 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.saveAndFlush(user);
     }
 }
